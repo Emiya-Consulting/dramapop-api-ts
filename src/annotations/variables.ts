0997887c9247ea4871444741eb5989db24bab76e
Rob Ranf <rob@emiyaconsulting.com>
2025-02-13 20:16:26 -0800

    gts and variables tests

---
+++ src/annotations/variables.ts
@@ -0,0 +1,38 @@
+let apples: number = 5;
+
+apples = 10;
+console.log(apples);
+
+const now: Date = new Date();
+console.log(now);
+
+const bandMembers: string[] = ["Jisoo", "Lisa", "Jennie", "Rosé"];
+for (let i = 0; i < bandMembers.length; i++) {
+	console.log(bandMembers[i]);
+}
+
+class Car {
+	make = "";
+	model = "";
+
+	constructor(make: string, model: string) {
+		this.make = make;
+		this.model = model;
+	}
+}
+
+const car: Car = new Car("Hyundai", "Tucson");
+console.log(car);
+
+const point: { x: number; y: number } = {
+	x: 10,
+	y: 20,
+};
+
+console.log(point);
+
+const logNumber = (i: number): void => {
+	console.log(i);
+};
+
+logNumber(12);
